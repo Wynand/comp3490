8bc6a77bbb4c66a348b5dad7a8644f7b02cf6db8
Wynand <wynandbadenhorst@hotmail.com>
2017-10-15 03:52:33 -0500

    added box supports with top, console, joystick, bin chute and guards.  Claw mechanisms are hierarchical.  4/10 complete

--- clawStart.js
+++ clawStart.js
@@ -48,113 +48,230 @@ function fillScene() {
 }
 
 function drawClawMachine() {
+	var clawMachine = makeClawMachine();
+	scene.add( clawMachine );
+} // drawClawMachine
 
-	//////////////////////////////
-	// Some simple material definitions - This may become more complex in A2
-
+function makeClawMachine(){
 	var arcadeMachine = new THREE.Group();
-	var frame = new THREE.Group();
-	var crane = new THREE.Group();
-	var cPanel = new THREE.Group();
-
 	var bodyMaterial = new THREE.MeshLambertMaterial();
 	bodyMaterial.color.setRGB( 0.5, 0.5, 0.5 );
 
-	var base;
+	var frame = makeFrame( bodyMaterial );
+	var crane = makeCrane( bodyMaterial ); // will move up/down
+		crane.position.y = 700; 
 
-	// This is where the model gets created. Add the appropriate geometry to create your machine
-	// You are not limited to using BoxGeometry, and likely want to use other types of geometry for pieces of your submission
-	// Note that the actual shape, size and other factors are up to you, provided constraints listed in the assignment description are met
-	
+	var cPanel = makeControlPanel( bodyMaterial );
+		cPanel.position.y = 300;
+		cPanel.position.z = -200;
 
- //The base
-	base = new THREE.Mesh(
-		new THREE.BoxGeometry( 300, 400, 300 ), bodyMaterial );
-	base.position.x = 0;
-	base.position.y = 200;
-	base.position.z = 0;
-	frame.add( base );
-	
-standShape = new THREE.BoxGeometry( 25, 400, 25);
+	var prizeBucket = makeChute( bodyMaterial );
+		prizeBucket.position.y = 420;
+		prizeBucket.position.x = -75;
+		prizeBucket.position.z = -75;
 
-// A supporting arms
-stand1 = new THREE.Mesh(
-	standShape , bodyMaterial );
-	stand1.position.x = 125;
-	stand1.position.y = 600;
-	stand1.position.z = 125;
-	frame.add( stand1 );
-
-stand2 = new THREE.Mesh(
-	standShape , bodyMaterial );
-	stand2.position.x = -125;
-	stand2.position.y = 600;
-	stand2.position.z = 125;
-	frame.add( stand2 );
-
-stand3 = new THREE.Mesh(
-	standShape , bodyMaterial );
-	stand3.position.x = -125;
-	stand3.position.y = 600;
-	stand3.position.z = -125;
-	frame.add( stand3 );
-
-stand4 = new THREE.Mesh(
-	standShape , bodyMaterial );
-	stand4.position.x = 125;
-	stand4.position.y = 600;
-	stand4.position.z = -125;
-	frame.add( stand4 );
-
-boxTop = new THREE.Mesh(
-	new THREE.BoxGeometry( 300, 50, 300 ), bodyMaterial );
-	boxTop.position.x = 0;
-	boxTop.position.y = 800;
-	boxTop.position.z = 0;
-	frame.add( boxTop );
-
-panel = new THREE.Mesh(
-	new THREE.BoxGeometry( 250, 20, 150), bodyMaterial );
-	panel.position.x = 0;
-	panel.position.y = 300;
-	panel.position.z = -150;
-	cPanel.add( panel );
-
-railShape = new THREE.BoxGeometry( 20, 20, 300 );
-
-railL = new THREE.Mesh(
-	railShape , bodyMaterial );
-	railL.position.x = 125;
-	railL.position.y = 700;
-	railL.position.z = 0;
-	crane.add( railL );
-
-railR = new THREE.Mesh(
-	railShape , bodyMaterial );
-	railR.position.x = -railL.position.x;
-	railR.position.y = railL.position.y;
-	railR.position.z = railL.position.z;
-	crane.add( railR );
-
-railArm = new THREE.Group();
-	
-railB = new THREE.Mesh(
-	new THREE.BoxGeometry( 300 , 20, 20 ), bodyMaterial );
-	panel.position.x = 0;
-	panel.position.y = 300;
-	panel.position.z = 0;
-	railArm.add( railB );
-
-	railArm.position.y = 700;
-
-	crane.add( railArm );
+	var prizeChute = makeChute( bodyMaterial );
+		prizeChute.rotateX(1);
+		prizeChute.position.y = 200;
+		prizeChute.position.x = -75;
+		prizeChute.position.z = -150;
 
 	arcadeMachine.add( frame );
 	arcadeMachine.add( crane );
-	arcadeMachine.add( cPanel );
+	arcadeMachine.add( cPanel ); 
+	arcadeMachine.add( prizeBucket );
+	arcadeMachine.add( prizeChute );
 
-	scene.add( arcadeMachine );
+	return arcadeMachine;
+}
 
+function makeControlPanel( bodyMaterial ){
+	var cPanel = new THREE.Group();	
+
+		panel = new THREE.Mesh( new THREE.BoxGeometry( 250, 20, 100), bodyMaterial );
+			panel.position.x = 0;
+			panel.position.y = 0;
+			panel.position.z = 0;
+		cPanel.add( panel );
+
+		joyStick = new THREE.Mesh( new THREE.BoxGeometry( 10, 80, 10), bodyMaterial );
+			joyStick.position.x = -75;
+			joyStick.position.y = 50;
+			joyStick.position.z = 0;
+		cPanel.add( joyStick );
+
+	return cPanel;
+}
+
+function makeFrame( bodyMaterial ){
+	var frame = new THREE.Group();
+
+		var base = new THREE.Mesh( new THREE.BoxGeometry( 300, 400, 300 ), bodyMaterial );
+			base.position.x = 0;
+			base.position.y = 200;
+			base.position.z = 0;
+		frame.add( base );
+		
+		stands = makeStands( bodyMaterial );
+			stands.position.y = 600;
+		frame.add( stands );
+		
+		glass = makeGlass();
+			glass.position.y = 600;
+		frame.add( glass );
+
+		boxTop = new THREE.Mesh(
+			new THREE.BoxGeometry( 300, 50, 300 ), bodyMaterial );
+			boxTop.position.x = 0;
+			boxTop.position.y = 800;
+			boxTop.position.z = 0;
+		frame.add( boxTop );
+
+	return frame;
+}
+
+function makeGlass(){
+	glassMaterial = new THREE.MeshLambertMaterial({transparent: true});
+		glassMaterial.opacity = 0.1;
+		glassMaterial.color.setRGB(70,130,180);
+
+	var glass = new THREE.Group();
+
+		dist = 148;
+		shapeFB = new THREE.BoxGeometry( 2 , 400 , 300 );
+		shapeLR = new THREE.BoxGeometry( 300 , 400 , 2 );
+
+		panef = new THREE.Mesh( shapeLR, glassMaterial );
+			panef.position.z = -dist;
+
+		paneb = new THREE.Mesh( shapeLR, glassMaterial );
+			paneb.position.z = dist;
+
+		panel = new THREE.Mesh( shapeFB, glassMaterial );
+			panel.position.x = -dist;
+
+		paner = new THREE.Mesh( shapeFB, glassMaterial );
+			paner.position.x = dist;
+
+		glass.add( panef );
+		glass.add( paneb );
+		glass.add( panel );
+		glass.add( paner );
+
+	return glass;
+
+}
+
+function makeChute( bodyMaterial ){
+	var chute = new THREE.Group();
+		var sideLR = new THREE.BoxGeometry( 4, 80, 80);
+		var sideFB = new THREE.BoxGeometry( 80, 80, 4);
+		var disp = 38;
+
+		var front = new THREE.Mesh( sideFB, bodyMaterial );
+			front.position.z = -disp;
+		chute.add( front );
+
+		var back = new THREE.Mesh( sideFB, bodyMaterial );
+			back.position.z = disp;
+		chute.add( back );
+
+		var left = new THREE.Mesh( sideLR, bodyMaterial );
+			left.position.x = -disp;
+		chute.add( left );
+
+		var right = new THREE.Mesh( sideLR, bodyMaterial );
+			right.position.x = disp;
+		chute.add( right );
+
+	return chute;
+}
+
+function makeStands( bodyMaterial ){
+	var bars = new THREE.Group();
+
+		var barShape = new THREE.BoxGeometry( 25, 400, 25);
+		disp = 125;
+		left = -disp;
+		right = disp;
+		front = -disp;
+		back = disp;
+
+		bar1 = new THREE.Mesh( barShape , bodyMaterial );
+			bar1.position.x = left;
+			bar1.position.z = front;
+
+		bar2 = new THREE.Mesh( barShape , bodyMaterial );
+			bar2.position.x = right;
+			bar2.position.z = front;
+
+		bar3 = new THREE.Mesh( barShape , bodyMaterial );
+			bar3.position.x = left;
+			bar3.position.z = back;
+
+		bar4 = new THREE.Mesh( barShape , bodyMaterial );
+			bar4.position.x = right;
+			bar4.position.z = back;
+
+		bars.add( bar1 );
+		bars.add( bar2 );
+		bars.add( bar3 );
+		bars.add( bar4 );
+
+	return bars;
+} 
+
+function makeCrane( bodyMaterial ){
+	var crane = new THREE.Group();
+
+		railShape = new THREE.BoxGeometry( 20, 20, 300 );
+
+		railL = new THREE.Mesh(
+			railShape , bodyMaterial );
+			railL.position.x = 125;
+			railL.position.y = 0;
+			railL.position.z = 0;
+		crane.add( railL );
+
+		railR = new THREE.Mesh(
+			railShape , bodyMaterial );
+			railR.position.x = -railL.position.x;
+			railR.position.y = railL.position.y;
+			railR.position.z = railL.position.z;
+		crane.add( railR );
+
+		railArm = makeRailArm( bodyMaterial ); // this will move forward and backward
+
+		crane.add( railArm );
+
+	return crane;
+}
+
+function makeRailArm( bodyMaterial ){
+
+	railArm = new THREE.Group();
+
+		railB = new THREE.Mesh(
+			new THREE.BoxGeometry( 300 , 20, 20 ), bodyMaterial );
+		railArm.add( railB );
+
+		hangingArm = makeHangingArm( bodyMaterial ); // this will move right/left
+			hangingArm.position.y = -20;
+		railArm.add( hangingArm );
+
+	return railArm;
+
+}
+
+function makeHangingArm( bodyMaterial ){
+	var hangingArm = new THREE.Group();	
+		var arm = new THREE.Mesh( new THREE.BoxGeometry( 15, 50, 15), bodyMaterial );
+			arm.position.y = -12;
+		hangingArm.add( arm );
+		var claw = new THREE.Mesh( new THREE.BoxGeometry( 30, 30, 30 ), bodyMaterial );
+			claw.position.y = -50;
+		hangingArm.add( claw );
+	return hangingArm;
 }
 
 // Initialization. Define the size of the canvas and store the aspect ratio
